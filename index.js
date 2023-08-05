3ca5544c9e03395e46b6da6341e12fad04757a83
Uzma Khan <91914613+Uzmakh@users.noreply.github.com>
2023-08-05 22:25:25 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
+function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let dayIndex = date.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[dayIndex];
+
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+    console.log(response);
+    document.querySelector("#city").innerHTML = response.data.name;
+    document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+    console.log(Math.round(response.data.main.temp));
+
+
+    // API  doesn't give us precipitation; Only when it is RAINING
+    // document.querySelector("#precipitation").innerHTML = response.data.main.precipitation;
+    document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+        response.data.wind.speed
+    );
     document.querySelector("#description").innerHTML =
         response.data.weather[0].description;
     console.log(response.data.wind.speed);
 }
-
 function searchCity(city) {
     let apiKey = "f420d385ec36bc3b2e50ca033dd04308";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
